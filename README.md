4b380b4f1629f45b20ad03e7d17b054574713a0c
Yury Bilenkis <yury.bilenkis@ostrovok.ru>
2015-12-29 12:14:42 +0300

    +README; move several task to funcions

---
+++ README.md
@@ -0,0 +1,41 @@
+Build OpenCV package for Raspbian
+=======================
+
+## Requirements
+
+1. Raspbian zipped image. You can download it from: https://downloads.raspberrypi.org/raspbian_latest
+2. OpenCV zipped sources ( for example from: https://github.com/Itseez/opencv/archive/3.1.0.zip)
+3. OpenCV modules ( cloned from: https://github.com/Itseez/opencv_contrib)
+4. run **python setup.py** with root. This need for **fdisk** and **mount**
+
+## Before run **setup.py**
+
+Make directory **opencv/** and copy downloaded opencv zipped sources. Cloned additional modules must be moved into this directory also.
+You must put Raspbian image (zipped or unzipped) into work directory where **setup.py** installed.
+
+Working directory must have this hierarcy:
+    - opencv/
+        - 3.1.0.zip
+        - opencv_contrib/
+    - mnt/
+    - chroot/
+    - rasbpian.zip
+
+## Notes
+
+Compiling is a long process. Please be patient.
+
+## List of references
+
+1. https://wiki.debian.org/QemuUserEmulation
+2. https://wiki.debian.org/RaspberryPi/qemu-user-static
+3. http://neophob.com/2014/10/run-arm-binaries-in-your-docker-container-using-boot2docker/
+4. https://github.com/dweinstein/dockerfile-qemu-arm-chroot
+5. http://blog.jenrom.com/2014/09/14/%D1%83%D1%81%D1%82%D0%B0%D0%BD%D0%BE%D0%B2%D0%BA%D0%B0-opencv-%D0%BD%D0%B0-linux/
+6. http://answers.opencv.org/questions/
+7. http://elinux.org/RPi_Software
+8. CrossCompiling:
+    - https://wiki.debian.org/CrossToolchains
+    - https://wiki.debian.org/ToolChain/Cross
+    - http://docs.opencv.org/2.4/doc/tutorials/introduction/crosscompilation/arm_crosscompile_with_cmake.html
+    - http://www.kitware.com/blog/home/post/426
--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ def imgprocessing( img ):
     m = re.search("\.img2\s+(\d+)\s+", p)
     offset = int(m.group(1)) * 512
     # mount img
+    p = Popen(['mkdir mnt'], shell=True)
+    p.wait()
     cmd = "mount -o loop,ro,offset=" + str(offset) + " " + img + " " + "mnt/"
     p = Popen([cmd], shell=True)
     p.wait()
@@ -17,6 +19,8 @@ def imgprocessing( img ):
     p = Popen(['rsync -a mnt/ chroot/'], shell=True)
     p.wait()
     p = Popen(['umount mnt/'], shell=True)
+    p.wait()
+    p = Popen(['rmdir mnt'], shell=True)
     p.wait()
     cmd = 'rsync -a opencv/' + "'" + opencvfile + "' " + 'chroot/usr/src/'
     p = Popen([cmd], shell=True)
@@ -62,7 +66,7 @@ def imgprocessing( img ):
                                             && cd /usr/src \
                                             && mkdir ' + opencvfile + '/build \
                                             && cd ' + opencvfile + '/build \
-                                            && cmake -D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules \
+                                            && cmake -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
                                                     -D CMAKE_BUILD_TYPE=RELEASE \
                                                     -D WITH_TBB=ON \
                                                     -D BUILD_NEW_PYTHON_SUPPORT=ON \
@@ -80,47 +84,56 @@ def imgprocessing( img ):
                                                     --pkgversion=' + opencv_version + ' \
                                                     --backup=no \
           "'
-    print "make: [%s]" % cmd
     p = Popen([cmd], shell=True)
 
 # check opencv
-if len(sys.argv) < 2:
-    print "1. Download opencv into dir [opencv/] like:\n" \
-            "\t [dir: opencv/]# curl -OLv https://github.com/Itseez/opencv/archive/3.1.0.zip\n" \
-            "2. Clone opencv modules into dir [opencv/opencv_contrib] like:\n" \
-            "\t [dir: opencv/]# git clone https://github.com/Itseez/opencv_contrib\n" \
-            "3. Run this setup.py once again with 'OK':\n" \
-            "\t # python setup.py OK\n"
-    exit(1)
+def check_opencv():
+    if len(sys.argv) < 2:
+        print "1. Download opencv into dir [opencv/] like:\n" \
+                "\t [dir: opencv/]# curl -OLv https://github.com/Itseez/opencv/archive/3.1.0.zip\n" \
+                "2. Clone opencv modules into dir [opencv/opencv_contrib] like:\n" \
+                "\t [dir: opencv/]# git clone https://github.com/Itseez/opencv_contrib\n" \
+                "3. Run this setup.py once again with 'OK':\n" \
+                "\t # python setup.py OK\n"
+        exit(1)
 
-if sys.argv[1] != "OK":
-    print "You must run: python setup.py OK"
-    exit(1)
+def check_ok():
+    if sys.argv[1] != "OK":
+        print "You must run: python setup.py OK"
+        exit(2)
 
 # unzip opencv
-os.chdir("opencv")
-zipfile = ''
-for line in os.listdir("."):
-    if re.search("\.zip$",line):
-        zipfile = line
-
-if len(zipfile):
-    m = re.search("(.+)\.zip$", zipfile)
-    opencv_version = m.group(1)
-    opencvfile = check_output(['unzip','-Z','-1', zipfile])
-    m = re.search('(.+)/',opencvfile)
-    opencvfile = m.group(1)
-    print "Extract to opencv/%s" % opencvfile
-    p = check_output(['unzip', '-u', zipfile ])
-os.chdir("..")
+def unzip_opencv():
+    os.chdir("opencv")
+    zipfile = ''
+    for line in os.listdir("."):
+        if re.search("\.zip$",line):
+            zipfile = line
+    
+    if len(zipfile):
+        m = re.search("(.+)\.zip$", zipfile)
+        opencv_version = m.group(1)
+        opencvfile = check_output(['unzip','-Z','-1', zipfile])
+        m = re.search('(.+)/',opencvfile)
+        opencvfile = m.group(1)
+        print "Extract to opencv/%s" % opencvfile
+        p = check_output(['unzip', '-u', zipfile ])
+    os.chdir("..")
+    return [opencv_version, opencvfile]
 
 # check qemu
-p = Popen(['test -e /usr/bin/qemu-arm-static'], shell=True)
-p.wait()
-if p.returncode:
-    print "Not found: /usr/bin/qemu-arm-static\nInstall it first like:\n" \
-            "apt-get install qemu qemu-user-static binfmt-support\n"
-    exit(1)
+def check_qemu():
+    p = Popen(['test -e /usr/bin/qemu-arm-static'], shell=True)
+    p.wait()
+    if p.returncode:
+        print "Not found: /usr/bin/qemu-arm-static\nInstall it first like:\n" \
+                "apt-get install qemu qemu-user-static binfmt-support\n"
+        exit(3)
+
+check_opencv()
+check_qemu()
+check_ok()
+opencv_version, opencvfile = unzip_opencv()
 
 # check RPi .zip | .img files
 imgfile, zipfile = ('', '')
@@ -141,4 +154,4 @@ if len(zipfile) and not len(imgfile):
     imgprocessing(unzipfile)
 if not len(imgfile) and not len(zipfile):
     print "Not found any .zip or .img file in image/. Exit."
-    exit(1)
+    exit(4)
